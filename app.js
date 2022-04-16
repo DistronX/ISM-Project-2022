4fce84f6bd2b11384ccf3ef4ff0f9904dc626230
Ambuj Gupta <ambujgupta2000@gmail.com>
2022-04-17 03:12:32 +0530

    Signup Page

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
-import './config/mongo.js';
+// import './config/mongo.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static('static/css'));
+app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
@@ -18,7 +18,7 @@ app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
   try{
-    res.render("login");
+    res.render("signup");
   }catch(err){
     console.log(err);
     res.status(500).send(err);
