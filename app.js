8ebad79119f04969da4fb27b3f5845bda71b7cdd
pavit1809 <pavitragoyal@hotmail.com>
2022-04-17 02:40:20 +0530

    Ejs setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 import './config/mongo.js';
 
@@ -8,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.json({limit: '10mb', extended: true}))
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('static/css'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 app.set("view engine","ejs");
@@ -17,7 +18,7 @@ app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
   try{
-    res.status(200).send('OK fine');
+    res.render("login");
   }catch(err){
     console.log(err);
     res.status(500).send(err);
